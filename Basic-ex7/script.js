367c8becf18c8fc5cb70382b6f73a6e61b2c21db
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-01 12:43:01 +0200

    add function to calculate area and volume

---
+++ Basic-ex7/script.js
@@ -0,0 +1,8 @@
+function areaAndVolume (width, height, depth){
+    const area = width * height;
+    const volume = width * height * depth;
+    console.log(`The area of the box is: ${area}`);
+    console.log(`The volume of the box is: ${volume}`);
+}
+
+areaAndVolume (5,4,3);
