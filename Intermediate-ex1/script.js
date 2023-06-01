fee29b03f04d54d6ab3261afebcba32b07928a3b
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-01 14:40:28 +0200

    improve function to be anonymous

--- Intermediate-ex1/script.js
+++ Intermediate-ex1/script.js
@@ -1,2 +1,7 @@
-const capitalize = string => console.log(string.charAt(0).toUpperCase() + string.slice(1)); 
-capitalize("hello would you look at this capitalized sentence?");
+/*const capitalize = string => console.log(string.charAt(0).toUpperCase() + string.slice(1)); 
+capitalize("hello would you look at this capitalized sentence?"); */
+
+const capitalize = (function() {  
+    let string = "hello would you look at this capitalized sentence?"
+    console.log(string.charAt(0).toUpperCase() + string.slice(1));
+  }());
