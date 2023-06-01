2e7f7550273a5be018eba95960cf2e8c24b14787
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-01 12:33:27 +0200

    improve to only pass one argument

--- Basic-ex4/script.js
+++ Basic-ex4/script.js
@@ -1,6 +1,6 @@
-function ageCalculator(birthYear, currentYear){
-    const older = currentYear - birthYear;
-    const younger = currentYear - birthYear - 1;
+function ageCalculator(birthYear){
+    const older = new Date().getFullYear() - birthYear;
+    const younger = new Date().getFullYear() - birthYear - 1;
     console.log(`You are either ${older} or ${younger} years old.`);
 }
-ageCalculator(1988, 2023);
+ageCalculator(1991);
