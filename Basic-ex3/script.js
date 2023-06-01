92e5b30eaa9642402f5a772b789e71010c6d0766
Lukas Kaiser <lukas.kaiser@kununu.com>
2023-06-01 10:39:33 +0200

    add a function that passes 4 parameters in a string

---
+++ Basic-ex3/script.js
@@ -0,0 +1,4 @@
+function crystalGazer (childrenAmount, partner, location, jobTitle) {
+   alert(`You will be a ${jobTitle} in ${location} and married to ${partner} with ${childrenAmount} children.`);
+}
+crystalGazer("5", "Maria", "China", "Salesman");
